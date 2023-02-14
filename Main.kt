b5f4f55a69e667b65b170d0c54569e2859d80cbf
Artem Demyanov <46056004+hantagu@users.noreply.github.com>
2023-02-14 23:57:10 +0300

    Лабораторная работа №8 по Прикладному программированию

--- Main.kt
+++ Main.kt
@@ -1,3 +1,111 @@
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import kotlin.math.exp
+import kotlin.random.Random
+import kotlin.system.exitProcess
+
 fun main()
 {
+    val rnd = Random(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC))
+
+    println("Матрицы:")
+    print("\tКоличество строк: ")
+    val rows = readln().toInt()
+
+    print("\tКоличество столбцов: ")
+    val columns = readln().toInt()
+
+    print("\tМножитель матриц (дробное число): ")
+    val coefficient = readln().toDouble()
+
+    print("\tСтепень, в которую будет возведены матрицы (целое число): ")
+    val exponent = readln().toInt()
+
+    println()
+
+    println("Диапазон генерации случайных значений")
+    print("\tНижняя граница (включительно): ")
+    val low = readln().toDouble()
+
+    print("\tВерхняя граница (не включительно): ")
+    val high = readln().toDouble()
+
+    println()
+
+    if (low >= high) {
+        println("Нижняя граница выше чем верхняя: [$low ; $high)")
+        exitProcess(1)
+    }
+
+    val a = Matrix(rows, columns) { _, _ -> rnd.nextDouble(low, high) }
+    val b = Matrix(rows, columns) { _, _ -> rnd.nextDouble(low, high) }
+
+    println("Матрица #1:\n$a\n")
+    println("Матрица #2:\n$b\n")
+
+    println("Сумма двух матриц:")
+    try {
+        println("${a + b}\n")
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}\n")
+    }
+
+    println("Разница двух матриц:")
+    try {
+        println("${a - b}\n")
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}\n")
+    }
+
+    println("Произведение матрицы #1 и $coefficient:\n${a * coefficient}\n")
+    println("Произведение матрицы #2 и $coefficient:\n${b * coefficient}\n")
+
+    println("Произведение двух матриц:")
+    try {
+        println("${a * b}\n")
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}\n")
+    }
+
+    println("Матрица #1 в степени $exponent:")
+    try {
+        println("${a pow exponent}\n")
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}\n")
+    }
+
+    println("Матрица #2 в степени $exponent:")
+    try {
+        println("${b pow exponent}\n")
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}\n")
+    }
+
+    println("Определитель матрицы #1:")
+    try {
+        println("${a.determinant()}\n")
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}\n")
+    }
+
+    println("Определитель матрицы #2:")
+    try {
+        println("${b.determinant()}\n")
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}\n")
+    }
+
+    println("Обратная матрица для матрицы #1:")
+    try {
+        println("${a.inverse()}\n")
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}\n")
+    }
+
+    println("Обратная матрица для матрицы #2:")
+    try {
+        println("${b.inverse()}\n")
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}\n")
+    }
 }
--- Matrix.kt
+++ Matrix.kt
@@ -1,67 +1,200 @@
+import java.util.*
+import kotlin.math.abs
+
 class Matrix(val rows: Int, val columns: Int)
 {
-    companion object {
-        sealed class Exception: Throwable() {
-            object InvalidMatrixSize: Exception()
-        }
-    }
-
     private val elements = MutableList(rows * columns) { 0.0 }
 
-    constructor(rows: Int, columns: Int, generator: (index: Int) -> Double) : this(rows, columns) {
+
+    constructor(rows: Int, columns: Int, generator: (row: Int, column: Int) -> Double) : this(rows, columns) {
+        for (i in elements.indices)
+            elements[i] = generator(i % columns, i / rows)
+    }
+
+    private constructor(rows: Int, columns: Int, generator: (index: Int) -> Double) : this(rows, columns) {
         for (i in elements.indices)
             elements[i] = generator(i)
     }
 
 
-
-    operator fun get(row: Int, column: Int) = this.elements[row * this.columns + column]
+    operator fun get(row: Int, column: Int) = elements[row * columns + column]
 
     operator fun set(row: Int, column: Int, value: Double) {
-        this.elements[row * this.columns + column] = value
+        elements[row * columns + column] = value
     }
-
 
 
     operator fun plus(other: Matrix): Matrix
     {
-        if (!(this.rows == other.rows && this.columns == other.columns))
-            throw Exception.InvalidMatrixSize
+        if (!(rows == other.rows && columns == other.columns))
+            throw IllegalArgumentException("Складывать допускается только матрицы одинаковой размерности")
 
-        return Matrix(this.rows, this.columns) { this.elements[it] + other.elements[it] }
+        return Matrix(rows, columns) { i -> elements[i] + other.elements[i] }
     }
-
-
 
     operator fun minus(other: Matrix): Matrix
     {
-        if (!(this.rows == other.rows && this.columns == other.columns))
-            throw Exception.InvalidMatrixSize
+        if (!(rows == other.rows && columns == other.columns))
+            throw IllegalArgumentException("Вычитать допускается только матрицы одинаковой размерности")
 
-        return Matrix(this.rows, this.columns) { this.elements[it] - other.elements[it] }
+        return Matrix(rows, columns) { i -> elements[i] - other.elements[i] }
     }
 
 
+    operator fun times(other: Double) = Matrix(rows, columns) { i -> elements[i] * other }
 
-    operator fun times(other: Int): Matrix
+    operator fun times(other: Matrix): Matrix
     {
-        return Matrix(this.rows, this.columns) { this.elements[it] * other }
+        if (columns != other.rows)
+            throw IllegalArgumentException("Умножать две матрицы допускается только в том случае, если число столбцов первой матрицы равняется числу строк второй матрицы")
+
+        val result = Matrix(rows, other.columns)
+
+        for (i in 0 until rows)
+        {
+            for (j in 0 until other.columns)
+            {
+                var sum = 0.0
+                for (k in 0 until columns)
+                    sum += elements[i * columns + k] * other.elements[k * other.columns + j]
+                result.elements[i * result.columns + j] = sum
+            }
+        }
+
+        return result
     }
 
+
+    infix fun pow(exponent: Int): Matrix
+    {
+        if (rows != columns)
+            throw IllegalArgumentException("Матрица не квадратная, нельзя возвести в степень")
+
+        if (exponent < 0)
+            throw IllegalArgumentException("Отрицательная степень не поддерживается")
+
+        var result = Matrix(rows, columns) { r, c -> if (r == c) 1.0 else 0.0 }
+
+        for (i in 0 until exponent)
+            result *= this
+
+        return result
+    }
+
+
+    private fun minor(rowToRemove: Int, columnToRemove: Int): Matrix
+    {
+        val minor = Matrix(rows - 1, columns - 1)
+
+        var minorIndex = 0
+        for (i in 0 until rows)
+        {
+            for (j in 0 until columns)
+            {
+                if (i != rowToRemove && j != columnToRemove)
+                {
+                    minor.elements[minorIndex] = elements[i * columns + j]
+                    minorIndex++
+                }
+            }
+        }
+
+        return minor
+    }
+
+
+    fun determinant(): Double
+    {
+        if (rows != columns)
+            throw IllegalArgumentException("Матрица не квадратная")
+
+        if (rows == 1)
+            return elements[0]
+
+        if (rows == 2)
+            return elements[0] * elements[3] - elements[1] * elements[2]
+
+        var det = 0.0
+        for (i in 0 until columns)
+        {
+            val minor = minor(0, i)
+            det += if (i % 2 == 0) elements[i] * minor.determinant() else -elements[i] * minor.determinant()
+        }
+
+        return det
+    }
+
+
+    fun inverse(): Matrix
+    {
+        if (rows != columns)
+            throw IllegalArgumentException("Матрица не квадратная")
+
+        val identity = Matrix(rows, columns) { r, c -> if (r == c) 1.0 else 0.0 }
+
+        val augmentedMatrix = Matrix(rows, columns * 2)
+        for (i in 0 until rows)
+        {
+            for (j in 0 until columns)
+                augmentedMatrix.elements[i * columns * 2 + j] = elements[i * columns + j]
+
+            for (j in 0 until columns)
+                augmentedMatrix.elements[i * columns * 2 + columns + j] = identity.elements[i * columns + j]
+        }
+
+        for (i in 0 until rows)
+        {
+            var maxRowIndex = i
+
+            for (j in i + 1 until rows)
+                if (abs(augmentedMatrix.elements[j * columns * 2 + i]) > abs(augmentedMatrix.elements[maxRowIndex * columns * 2 + i]))
+                    maxRowIndex = j
+
+            if (augmentedMatrix.elements[maxRowIndex * columns * 2 + i] == 0.0)
+                throw IllegalArgumentException("Обратной матрицы не существует")
+
+            if (maxRowIndex != i)
+                for (j in 0 until columns * 2)
+                {
+                    val temp = augmentedMatrix.elements[i * columns * 2 + j]
+                    augmentedMatrix.elements[i * columns * 2 + j] = augmentedMatrix.elements[maxRowIndex * columns * 2 + j]
+                    augmentedMatrix.elements[maxRowIndex * columns * 2 + j] = temp
+                }
+
+            val pivot = augmentedMatrix.elements[i * columns * 2 + i]
+            for (j in 0 until columns * 2)
+                augmentedMatrix.elements[i * columns * 2 + j] /= pivot
+
+            for (j in 0 until rows)
+                if (j != i)
+                {
+                    val factor = augmentedMatrix.elements[j * columns * 2 + i]
+                    for (k in 0 until columns * 2)
+                        augmentedMatrix.elements[j * columns * 2 + k] -= factor * augmentedMatrix.elements[i * columns * 2 + k]
+                }
+        }
+
+        val inverse = Matrix(rows, columns)
+        for (i in 0 until rows)
+            for (j in 0 until columns)
+                inverse.elements[i * columns + j] = augmentedMatrix.elements[i * columns * 2 + columns + j]
+
+        return inverse
+    }
 
 
     override fun toString(): String
     {
         val stringBuilder = StringBuilder()
 
-        for (row in 0 until this.rows)
+        for (r in 0 until rows)
         {
-            for (column in 0 until this.columns) {
-                stringBuilder.append(this[row, column])
+            for (c in 0 until columns) {
+                stringBuilder.append("%.2f".format(Locale.US, this[r, c]))
                 stringBuilder.append("\t")
             }
 
-            if (row != this.rows - 1)
+            if (r != rows - 1)
                 stringBuilder.append("\n")
         }
 
